8d0d8f9bbaf6681c25ca7e438ccfca201d548cd6
tarlada1981 <ktarlada@gmail.com>
2026-01-08 10:30:57 -0500

    Update kb_engine.py

--- kb_engine.py
+++ kb_engine.py
@@ -1,5 +1,5 @@
 from excel_db import read_table
 
 def search_kb(problem):
-    df = read_table("kb_knowledge")
+    df = read_table("Knowledgebase")
     return df[df["short_description"].str.contains(problem, case=False)]
