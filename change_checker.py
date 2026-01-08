4c856e652158df9502f7f238b2517a9b4af83e9f
tarlada1981 <ktarlada@gmail.com>
2026-01-08 10:29:06 -0500

    Update change_checker.py

--- change_checker.py
+++ change_checker.py
@@ -3,7 +3,7 @@ from excel_db import read_table
 from datetime import datetime, timedelta
 
 def check_recent_changes(ci):
-    df = read_table("change_request")
+    df = read_table("Change")
     df["start_date"] = pd.to_datetime(df["start_date"])
 
     last_24h = datetime.now() - timedelta(hours=24)
